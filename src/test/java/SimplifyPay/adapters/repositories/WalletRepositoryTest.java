fbf69650a76640a8767624c1930dcf17b8bf8c90
Miguel Delgado <delgdevoleper@gmail.com>
2024-12-20 22:09:50 -0300

    feature: configurado database posgres para teste
    para fazer essa configuracao eu fui no Postbird e criei um novo database com o mesmo nome do meu database de producao, PROD: postgres | TEST: postgres_test

---
+++ src/test/java/SimplifyPay/adapters/repositories/WalletRepositoryTest.java
@@ -0,0 +1,19 @@
+package SimplifyPay.adapters.repositories;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
+class WalletRepositoryTest {
+
+
+    @Test
+    void findByUserId() {
+    }
+}
